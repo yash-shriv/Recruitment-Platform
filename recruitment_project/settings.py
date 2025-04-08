6552fcac6e7ca6c59b937c58575689613e11bc8a
Yash Shrivastava <115723600+yash-shriv@users.noreply.github.com>
2025-04-08 11:51:56 +0530

    Update settings.py

--- recruitment_project/settings.py
+++ recruitment_project/settings.py
@@ -143,12 +143,6 @@ CELERY_BROKER_URL = "redis://localhost:6379/0"  # Using Redis as broker
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
-# EMAIL_HOST = os.getenv("EMAIL_HOST")
-# EMAIL_PORT = int(os.getenv("EMAIL_PORT"), 587)
-# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
 #1
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
